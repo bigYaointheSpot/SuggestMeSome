90a7ef0134761c2c89c29546766b1576e8458968
ayao7378 <alexanderyao11@gmail.com>
2026-04-05 13:48:30 -0400

    Initial Commit

---
+++ SuggestMeSomeTests/SuggestMeSomeTests.swift
@@ -0,0 +1,19 @@
+//
+//  SuggestMeSomeTests.swift
+//  SuggestMeSomeTests
+//
+//  Created by Alex Yao on 4/5/26.
+//
+
+import Testing
+@testable import SuggestMeSome
+
+struct SuggestMeSomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
